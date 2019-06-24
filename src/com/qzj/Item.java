8dc806187d337781615d91abe821441e48e018b4
blingbling-110 <523497359@qq.com>
2019-06-24 23:07:43 +0800

    新建Item公共类

---
+++ src/com/qzj/Item.java
@@ -0,0 +1,34 @@
+package com.qzj;
+
+public class Item {// Item公共类：对id和name属性进行封装
+	private String id;
+	private String name;
+	
+	public Item() {// 默认构造方法
+	}
+	
+	public Item(String id, String name) {// 包含所有属性初始化的构造方法
+		this.id = id;
+		this.name = name;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String toString() {// 重写toString()方法，只能输出name属性
+		return getName();
+	}
+}
