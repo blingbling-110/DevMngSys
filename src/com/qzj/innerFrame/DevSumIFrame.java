d9188827da9df0735ef926bbe05c6bef5a54d0f6
blingbling-110 <523497359@qq.com>
2019-07-08 22:36:44 +0800

    了解表格模型

--- src/com/qzj/innerFrame/DevSumIFrame.java
+++ src/com/qzj/innerFrame/DevSumIFrame.java
@@ -3,6 +3,7 @@ package com.qzj.innerFrame;
 import javax.swing.JInternalFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 
 /**
  * 	设备总览内部窗体
@@ -33,5 +34,19 @@ public class DevSumIFrame extends JInternalFrame {
 		setResizable(true);//	窗体可拉伸
 		setIconifiable(true);//	窗体可图标化
 		setClosable(true);//	窗体可关闭
+		tablePane = new JScrollPane();
+		tablePane.setViewportView(getTable());
+		getContentPane().add(tablePane);
+	}
+
+	public JTable getTable() {
+		table = new JTable();
+		table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);//	关闭列的自动调节
+		String[] columnLabel = {"设备编号", "设备名称", "设备状态", "设备描述", "备注"};
+		//	DefaultTableModel是用于保存表格单元数值的表格模型类
+		DefaultTableModel model = (DefaultTableModel) table.getModel();
+		model.setColumnIdentifiers(columnLabel);//	设置表格模型的列名称
+		
+		return table;
 	}
 }
